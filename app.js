173acbf93e111422a4aad9f211627f282df5134e
Menimobong Utibe-Ukim <menimobong@berkeley.edu>
2026-05-05 09:35:41 -0700

    Add styles to the views

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const PORT = process.env.PORT || 3000;
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
 app.use("/new", newRouter);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (error) => {
     if (error) {
